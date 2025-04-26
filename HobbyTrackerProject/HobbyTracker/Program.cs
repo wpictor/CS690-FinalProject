1c43f0edb8375c7063cc514889cd6d4b0d5c4ed9
wpictor <wpictor@gmail.com>
2025-04-26 16:21:24 +0000

    refined code with methods

--- HobbyTrackerProject/HobbyTracker/Program.cs
+++ HobbyTrackerProject/HobbyTracker/Program.cs
@@ -1,109 +1,110 @@
 ﻿namespace HobbyTracker;
 
 using System.IO;
-class Program
+using Spectre.Console;
+
+class Program   
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please select a user (Oscar or New User):");
-        string user = Console.ReadLine();
-        
+        string user = fancySelect("Please select a user: ", ["Oscar"]);
+        consoleUI(user);
+    }
+
+    static string fancySelect(string message, List<string> selectList)
+    {
+        var response = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title(message)
+                .AddChoices(selectList)
+                .AddChoices("Back"));
+        return response;
+    }
+    static string fancySelectActivity(string message, List<string> selectList)
+    {
+        var response = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title(message)
+                .AddChoices(selectList));
+        return response;
+    }
+
+    static string getUserInput(string message)
+    {
+        var response = AnsiConsole.Prompt(
+            new TextPrompt<string>(message));
+        return response;
+    }
+
+    static void runReport()
+    {
+        string hobbyListContents = File.ReadAllText("hobby-sheet.txt");
+        Console.WriteLine("*********************************");
+        foreach(string hobbyLine in hobbyListContents.Split('\n')){
+            if(hobbyLine!="" & File.Exists(hobbyLine+".txt")){                                
+                string activityListContents = File.ReadAllText(hobbyLine + ".txt");
+                Console.WriteLine("Hobby: " + hobbyLine);
+                Console.WriteLine("Activities:");
+                foreach(string activityLine in activityListContents.Split('\n')){
+                    if(activityLine != ""){
+                        Console.WriteLine(activityLine);
+                    }
+                }
+                Console.WriteLine("*********************************");
+            }
+        }
+    }
+    static void consoleUI(string user)
+    {
         if(user == "Oscar"){
             string firstScreenOption;
             Console.WriteLine("Welcome " + user + "!");
-
             do{
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("Please select from the following:");
-                Console.WriteLine("View Hobbies (type view)");
-                Console.WriteLine("Add New Hobby (type add)");
-                Console.WriteLine("Run Hobby Report (type report)");
-                Console.WriteLine("Sign out (type end)");
-                Console.WriteLine("---------------------------------");
-
-                firstScreenOption = Console.ReadLine();
-
-                if(firstScreenOption == "view"){
+                firstScreenOption = fancySelect("\nPlease select from the following:", ["View Hobbies", "Add New Hobby", "Run Hobby Report"]);
+                if(firstScreenOption == "View Hobbies"){
                     string secondScreenOption;
                     string hobbyListContents = File.ReadAllText("hobby-sheet.txt");
                     List<string> hobbyList = new List<string>();
-                    do{
-                        Console.WriteLine("---------------------------------");
-                        Console.WriteLine("You have the following Hobbies:");
-                        //Console.WriteLine(hobbyListContents);
-                        foreach(string hobbyLine in hobbyListContents.Split('\n')){
+                    foreach(string hobbyLine in hobbyListContents.Split('\n')){
                             if(hobbyLine!=""){
                                 Console.WriteLine(hobbyLine);
                                 hobbyList.Add(hobbyLine);
                             }
                         }
-                        Console.WriteLine("---------------------------------");
-                        Console.WriteLine("Please select a hobby to continue:");
-                        Console.WriteLine("Enter hobby name exactly how it appears above:");
-                        Console.WriteLine("Or enter 'back' to return to main menu");
-                        secondScreenOption = Console.ReadLine();
+                    do{
+                        secondScreenOption = fancySelect("\nPlease select a hobby to continue: ", hobbyList);
                         if(hobbyList.Contains(secondScreenOption)){
                             string thirdScreenOption;
                             do{
-                                Console.WriteLine("---------------------------------");
-                                Console.WriteLine("You've selected " + secondScreenOption);
-                                Console.WriteLine("What would you like to do?");
-                                Console.WriteLine("Add New Activity (type add)");
-                                //Console.WriteLine("Delete Activity (type delete)");
-                                Console.WriteLine("Go Back (type back)");
-                                Console.WriteLine("---------------------------------");
-                                thirdScreenOption = Console.ReadLine();
-                                string newActivity;
-                                string newPriority;
-                                if(thirdScreenOption == "add"){
-                                    Console.WriteLine("Enter Activity:");
-                                    newActivity = Console.ReadLine();
-                                    Console.WriteLine("Enter priority as 'High' 'Medium' or 'Low':");
-                                    newPriority = Console.ReadLine();
-                                    File.AppendAllText(secondScreenOption+".txt", newActivity + " - " + newPriority + Environment.NewLine);
-                                }//else if(thirdScreenOption == "delete"){
-                                    
-                                //}
-
-
-                            }while(thirdScreenOption != "back");
-
-                            //File.AppendAllText(secondScreenOption+".txt", "testing");
+                                thirdScreenOption = fancySelect("\nYou've selected '" + secondScreenOption + "'\nWhat would you like to do?", ["Add Activity"]);
+                                if(thirdScreenOption == "Add Activity"){
+                                    Activity addActivity = new Activity();
+                                    addActivity.name = getUserInput("Enter an Activity: ");
+                                    addActivity.priority = fancySelectActivity("Select Priority", ["High", "Medium", "Low"]);
+                                    File.AppendAllText(secondScreenOption+".txt", addActivity.name + " - " + addActivity.priority + Environment.NewLine);
+                                }
+                            }while(thirdScreenOption != "Back");
                         }
-                        //else{
-                        //    Console.WriteLine("Enter a valid hobby to continue.");
-                        //}
-                    }while(secondScreenOption != "back");
+                    }while(secondScreenOption != "Back");
                 }
-                else if(firstScreenOption == "add"){
-                    Console.WriteLine("Please type the name of your new hobby:");
-                    string hobby = Console.ReadLine();
+                else if(firstScreenOption == "Add New Hobby"){
+                    string hobby = getUserInput("Please type the name of your new hobby:");
                     File.AppendAllText("hobby-sheet.txt", hobby+Environment.NewLine);
                 }
-                else if(firstScreenOption == "report"){
-                    string hobbyListContents = File.ReadAllText("hobby-sheet.txt");
-                    Console.WriteLine("*********************************");
-                    foreach(string hobbyLine in hobbyListContents.Split('\n')){
-                            if(hobbyLine!="" & File.Exists(hobbyLine+".txt")){                                
-                                string activityListContents = File.ReadAllText(hobbyLine + ".txt");
-                                Console.WriteLine("Hobby: " + hobbyLine);
-                                Console.WriteLine("Activities:");
-                                foreach(string activityLine in activityListContents.Split('\n')){
-                                    if(activityLine != ""){
-                                        Console.WriteLine(activityLine);
-                                    }
-                                }
-                                Console.WriteLine("*********************************");
-                            }
-                        }
+                else if(firstScreenOption == "Run Hobby Report")
+                {
+                    runReport();    
                 }
                 else{
                     Console.WriteLine("You have succesfully signed out.");
                 }
-            }while(firstScreenOption != "end");
-            
-
+            }while(firstScreenOption != "Back");
         }
-        
     }
+}
+
+class Activity
+{
+    public string name; 
+    public string priority;
 }
